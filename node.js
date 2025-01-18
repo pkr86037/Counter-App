2feae0b3082496bf00025361dec94fd4a65f118e
pkr86037 <pkr86037@gmail.com>
2025-01-18 07:49:05 +0530

    add new feature

--- node.js
+++ node.js
@@ -2,6 +2,7 @@ const count = document.querySelector('.count');
 const minusBtn = document.querySelector('.minus');
 const plusBtn = document.querySelector('.plus');
 const resetBtn = document.querySelector('.reset');
+const classby = document.querySelector('.classby');
 
 
 minusBtn.addEventListener('click', () => {
